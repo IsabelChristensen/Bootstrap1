fdcfc8f6a17b4fb95168f84acdd3cd036032ad0e
Johanne Jungild <johannejungild@gmail.com>
2017-03-27 12:56:03 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,17 @@
+<!--Footer-->
+<footer><div class="container">
+<div class="row">
+		<a href="http://www.profil-rejser.dk/arktiske-omraader/svalbard/rejser/rundt-om-spitsbergen"><img src="images/profilrejser_logo.png" alt="" class="col-xs-2"><img src="images/logohvid.png" alt="" class="col-xs-1"></a>
+		<div class="col-xs-2"><h4>Tlf.: +45 12345678</h4>
+<h4>E-mail: profilrejser@mail.dk</h4></div>
+		
+		<div class="col-xs-4">
+		<p>Dette kampangesite er lavet som projektopgave baseret på Profil Rejsers allerede eksisterende rejse til Svalbard, og hjemmesiden kan derfor ikke sættes i forbindelse med Profil Rejser.</p>
+	</div>
+	<div class="col-xs-3">
+		<a href="http://www.profil-rejser.dk/arktiske-omraader/svalbard/rejser/rundt-om-spitsbergen">Her kan I læse mere om Profil Rejser og den konkrete rejse, som vi har arbejdet med.</a>
+	</div>
+</div>
+	
+</div>
+</footer>
